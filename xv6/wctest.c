40f30ad8587201c92de6fe70398c3a6f3e735fce
Alexandros Pfoser <ap4gf@virginia.edu>
2023-09-21 16:15:40 -0400

    sub

--- xv6/wctest.c
+++ xv6/wctest.c
@@ -13,10 +13,19 @@ main(int argc, char *argv[])
     setwritecount(150);
     printf(1, "After setting to 150: %d\n", writecount());
     printf(1, "printf() should increase writecount: %d\n", writecount());
+    printf(1, "because it calls write()\n");
+    printf(1, "but, the function writecount() is called before printf's write()\n");
+    printf(1, "total amount of writes(), ecxcluding this printf: %d\n", writecount());
 
     printf(1, "---Resetting to 0---\n");
     setwritecount(0);
     printf(1, "After resetting: %d\n", writecount());
+    printf(1, "Testing value after 0-reset: %d\n", writecount());
+    
+    printf(1, "---Resetting to 99---\n");
+    setwritecount(99);
+    printf(1, "After resetting: %d\n", writecount());
+    printf(1, "Should be over 100: %d\n", writecount());
 
 
     exit();
